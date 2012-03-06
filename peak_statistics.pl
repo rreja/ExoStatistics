af8017c8c1c2cca91a2e9044938663a0c3993bcb
rreja <rejarohit2004@gmail.com>
2012-03-06 15:43:44 -0500

    perl script for stats

---
+++ peak_statistics.pl
@@ -0,0 +1,69 @@
+use strict;
+use warnings;
+use Getopt::Std;
+use File::Basename;
+
+
+
+# Decalre all the global variables
+my %options;
+my (@tagcounts,@stds);
+my ($stddev); my $peakcount = 0; my $singletoncount= 0;
+
+getopt('ib',\%options);
+
+
+# Using fileparser to get the basename and path
+my ($fname,$path,$suffix) = fileparse($ARGV[0],".gff");
+my $basename = basename($ARGV[0], ".gff");
+
+open IN,$ARGV[0] || "Input file not found\n";
+open OUT,">".$path.$basename."_NoS.gff" || "Output file not found"; # change here if you want to change the output file directory
+
+
+while(<IN>){
+    
+    chomp($_);
+# Extracting stddev from file based on the 9th column, if it has ';' seperation or not
+    my @cols = split(/\t/,$_);
+    if($cols[8] =~ m/.*;.*/){
+        foreach my $val (split(/;/,$cols[8])){
+            if((split(/=/,$val))[0] eq "stddev"){
+                $stddev = (split(/=/,$val))[1];
+                last;
+            }
+        }
+    }
+    elsif((split(/=/,$cols[8]))[0] eq "stddev"){
+        $stddev = (split(/=/,$cols[8]))[1];
+        
+    }
+    
+    if($stddev > 0.0){
+        print $cols[5]."\n";
+        print OUT $_."\n";
+        $peakcount++;
+        push(@tagcounts,$cols[5]);
+        push(@stds,$stddev);
+    }
+    else{
+        $singletoncount++;
+        
+    }
+    
+}
+
+
+
+sub median{
+    my @a = sort {$a <=> $b} @_;
+  return ($a[$#a/2] + $a[@a/2]) / 2;
+}
+
+sub mean{
+  @_ or return 0;
+  my $sum = 0;
+  $sum += $_ foreach @_;
+  return $sum/@_;
+}
+
