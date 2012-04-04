c1a01ce241a46ebcbe0d618c69e43c9d9740d3a3
Rohit <rejarohit2004@gmail.com>
2012-04-04 12:46:55 -0400

    changed script. no shell required

--- peak_statistics.pl
+++ peak_statistics.pl
@@ -1,26 +1,37 @@
 use strict;
 use warnings;
 use File::Basename;
+use Getopt::Std;
 
+my %opt;
+getopt('i',\%opt);
+&help_message if (defined $opt{h});
 
 # Decalre all the global variables
 my %options;
 my (@tagcounts,@stds);
 my ($stddev); my $peakcount = 0; my $singletoncount= 0;
 
+my $dir = $opt{'i'}; #remember to put a "/" at the end of the directory.
+opendir DIR, $dir || die "Cannot open the directory";
+$dir = check_dir($dir);
+
+open OUT1, ">".$dir."peak_stats.txt" || die "File not found"; # the file containing the summary
+
+while( (my $filename = readdir(DIR))){
+
+next if($filename =~ /^\./);
 # Using fileparser to get the basename and path
-my ($fname,$path,$suffix) = fileparse($ARGV[0],".gff");
-my $basename = basename($ARGV[0], ".gff");
+my ($fname,$path,$suffix) = fileparse($filename,".gff");
+my $basename = basename($filename, ".gff");
 
-open IN,$ARGV[0] || "Input file not found\n";
+if($suffix eq ".gff"){
 
-## ------comment out the line below out when running this script on a single file.-------
-open OUT,">".$path."output/".$basename."_NoS.gff" || "Output file not found"; # change here if you want to change the output file directory
 
-##------Uncomment these two line when running on a single file------
-#open OUT, ">".$path.$basename."_NoS.gff" || "Output file not found";
-#print "Filename\tMapped_reads\tUniquely_mapped_reads\tPeaks\tSingletons\tPeak_median_excluding_singletons\tPeak_mean_exclusing_singletons\tMedian_std_excluding_singeltons\tMean_std_excluding_singletons\n";
+open IN,$dir.$filename || die "Input file not found\n";
+open OUT, ">".$dir.$basename."_NoS.gff" || die "Output file not found";
 
+print OUT1 "Filename\tMapped_reads\tUniquely_mapped_reads\tPeaks\tSingletons\tPeak_median_excluding_singletons\tPeak_mean_exclusing_singletons\tMedian_std_excluding_singeltons\tMean_std_excluding_singletons\n";
 while(<IN>){
     
     chomp($_);
@@ -54,7 +65,14 @@ while(<IN>){
    
 }
 
-print $basename.".gff"."\t.\t.\t".$peakcount."\t".$singletoncount."\t".median(@tagcounts)."\t".mean(@tagcounts)."\t".median(@stds)."\t".mean(@stds)."\n";
+print OUT1 $basename.".gff"."\t.\t.\t".$peakcount."\t".$singletoncount."\t".median(@tagcounts)."\t".mean(@tagcounts)."\t".median(@stds)."\t".mean(@stds)."\n";
+close(OUT);
+close(IN);
+ }
+
+}
+
+
 
 sub median{
     my @a = sort {$a <=> $b} @_;
@@ -67,4 +85,45 @@ sub mean{
   $sum += $_ foreach @_;
   return $sum/@_;
 }
+
+sub check_dir{
+    my $dir = shift;
+    my $tmp = substr($dir,-1,1);
+    if($tmp eq "/"){
+        return($dir);
+    }
+    else{
+        return($dir."/");
+    }
+}
+
+sub help_message {
+  print qq{
+Program: peak_statistics.pl (Calculate stats on peak calls)
+Contact: Rohit Reja <rzr142\@psu.edu>
+Usage:   peak_statistics.pl -i <peak_calls_file_directory>
+
+    NOTE:    If you input files were saved using MS excel, then use:  perl -p -e 's/^M//g;' <input_file> > <input_file_no_excel_characters>
+             to remove the excel characters in your file. ^M sould be typed as "ctrl-v-m". Or else the script will not work properly. Peak
+             calls should be in "gff" format.
+             
+    Options: -i <path1>     path to the folder with peak call files [accepted file format, gff].
+             -h             help 
+
+    Example:
+      perl peak_statistics.pl -i  .  [if your files are in the current directory]
+      perl robust_peak_pair_stats.pl -i genetrack_s5e10F1/cwpair_output_mode_f0u0d100b3
+      
+    Output:
+    Produces a "peak_stats.txt" file and also produces a "_NoS" file that contains all the
+    non-singelton peaks in it. These files will be present in the folder that contains the peak call files.
+  
+  };
+  exit;
+}
+
+
+
+
+
 
